4585bc06c896c956df7900ad2d42bede8aa08df5
Justin Bull <me@justinbull.ca>
2013-11-06 21:10:39 -0500

    Create an empty Rails 4.0.1 project

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webapp::Application.config.secret_key_base = '48e60030589029a0057b8bd9e5731ba116e24b8502500657013d19051afb50ee2725d95d29335ff4a283d2f33855a42b2869c175781bc26b0e79f00a288b688e'
