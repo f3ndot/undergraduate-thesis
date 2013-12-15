a3831fcdd2e1bbd9ae5be14a155f23b3de0d1b47
Justin Bull <me@justinbull.ca>
2013-12-15 15:57:59 -0500

    Remove scaffold CSS and build .active nav link helper

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nav_link(name, path)
+    content = link_to name, path
+    if current_page? path
+      content_tag :li, content, class: 'active'
+    else
+      content_tag :li, content
+    end
+  end
+
 end
